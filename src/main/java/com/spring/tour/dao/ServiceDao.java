02ff306cfe2adbcb6269a60b52efe3bae0b6499b
Egregoros <43488826+Egregoros@users.noreply.github.com>
2021-02-03 14:45:19 +0900

    .

--- src/main/java/com/spring/tour/dao/ServiceDao.java
+++ src/main/java/com/spring/tour/dao/ServiceDao.java
@@ -4,13 +4,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.spring.tour.vo.User_InfoVo;
 
 @Repository
 public class ServiceDao {
 	@Autowired private SqlSession sqlSession;
 	private static String NAMESPACE="com.test.spring.tour.mapper.ServiceMapper";
-	public User_InfoVo getinfo(String user_id) {
-		return sqlSession.selectOne(NAMESPACE+".getinfo",user_id);
-	}
+	
 }
--- src/main/java/com/spring/tour/service/ServiceService.java
+++ src/main/java/com/spring/tour/service/ServiceService.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.tour.dao.ServiceDao;
-import com.spring.tour.vo.User_InfoVo;
 @Service
 public class ServiceService {
 	@Autowired private ServiceDao dao;
-	public User_InfoVo select(String user_id) {
-		return dao.getinfo(user_id);
-	}
+	
 }
