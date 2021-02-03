e2fd4f4b4a07de2c4c6ac20b07d1c6550c36dc17
Egregoros <43488826+Egregoros@users.noreply.github.com>
2021-02-03 14:43:34 +0900

    service mapper

--- src/main/java/com/spring/tour/dao/ServiceDao.java
+++ src/main/java/com/spring/tour/dao/ServiceDao.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Repository;
 import com.spring.tour.vo.User_InfoVo;
 
 @Repository
-public class MyPageDao {
+public class ServiceDao {
 	@Autowired private SqlSession sqlSession;
-	private static String NAMESPACE="com.test.spring.tour.mapper.MypageMapper";
+	private static String NAMESPACE="com.test.spring.tour.mapper.ServiceMapper";
 	public User_InfoVo getinfo(String user_id) {
 		return sqlSession.selectOne(NAMESPACE+".getinfo",user_id);
 	}
