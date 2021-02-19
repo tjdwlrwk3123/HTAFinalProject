0f2ad80899db5036235c5dbad26f3c23e205649a
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-02-19 16:36:15 +0900

    테이블 컬럼용량 늘렸어요

--- src/test1.sql
+++ src/test1.sql
@@ -54,7 +54,7 @@ CREATE TABLE accom_option
 (
 	accom_option_number number NOT NULL,
 	accom_service_number number NOT NULL,
-	accom_rooms_option varchar2(40),
+	accom_rooms_option varchar2(1000),
 	accom_min_people number,
 	accom_max_people number,
 	accom_price number,
@@ -67,8 +67,8 @@ CREATE TABLE accom_service
 (
 	accom_service_number number NOT NULL,
 	cate_number number NOT NULL,
-	user_accom_boss_name varchar2(30),
-	accom_name varchar2(50),
+	user_accom_boss_name varchar2(500),
+	accom_name varchar2(500),
 	accom_addr clob,
 	PRIMARY KEY (accom_service_number)
 );
@@ -77,16 +77,16 @@ CREATE TABLE accom_service
 CREATE TABLE accom_book
 (
 	accom_book_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	accom_option_number number,
-	service_name varchar2(100),
+	service_name varchar2(1000),
 	accom_startdate date,
 	accom_enddate date,
-	payment_condition varchar2(30),
+	payment_condition varchar2(200),
 	total_price number,
 	point_useamount number,
-	coupon_usecondition varchar2(50),
-	payment_method varchar2(50),
+	coupon_usecondition varchar2(200),
+	payment_method varchar2(200),
 	PRIMARY KEY (accom_book_number)
 );
 
@@ -94,18 +94,18 @@ CREATE TABLE accom_book
 CREATE TABLE tour_book
 (
 	tour_book_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	service_number number(5), --tour_service에서 가져온 service_number
-	service_name varchar2(100),
+	service_name varchar2(1000),
 	tour_startdate date,
 	tour_enddate date,
-	payment_condition varchar2(30),
+	payment_condition varchar2(200),
 	total_price number,
 	point_useamount number,
-	coupon_usecondition varchar2(50),
-	payment_method varchar2(50),
-	bookername varchar2(50),
-	bookerphone varchar2(100),
+	coupon_usecondition varchar2(200),
+	payment_method varchar2(200),
+	bookername varchar2(200),
+	bookerphone varchar2(200),
 	PRIMARY KEY (tour_book_number)
 );
 
@@ -123,7 +123,7 @@ CREATE TABLE tour_book_option
 CREATE TABLE category
 (
 	cate_number number NOT NULL,
-	cate_name varchar2(30),
+	cate_name varchar2(200),
 	PRIMARY KEY (cate_number)
 );
 
@@ -132,7 +132,7 @@ CREATE TABLE coupon
 (
 	coupon_number number NOT NULL,
 	coupon_name varchar2(255) NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	discount_price number,
 	expire_date date,
 	coupon_usecondition number(5),
@@ -143,8 +143,8 @@ CREATE TABLE coupon
 CREATE TABLE customercenter
 (
 	question_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
-	question_content varchar2(100),
+	user_id varchar2(200) NOT NULL,
+	question_content varchar2(2000),
 	question_date date,
 	PRIMARY KEY (question_number)
 );
@@ -154,7 +154,7 @@ CREATE TABLE customercenter_answer
 (
 	answer_number number NOT NULL,
 	question_number number NOT NULL,
-	answer_content varchar2(100),
+	answer_content varchar2(2000),
 	answer_date date NOT NULL,
 	PRIMARY KEY (answer_number)
 );
@@ -163,7 +163,7 @@ CREATE TABLE customercenter_answer
 CREATE TABLE id_token
 (
 	token_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	token_data clob,
 	token_authority clob,
 	PRIMARY KEY (token_number)
@@ -173,8 +173,8 @@ CREATE TABLE id_token
 CREATE TABLE image
 (
 	img_num number NOT NULL,
-	imgorgname varchar2(30) NOT NULL,
-	imgsavename varchar2(30) NOT NULL,
+	imgorgname varchar2(3000) NOT NULL,
+	imgsavename varchar2(3000) NOT NULL,
 	general_number number NOT NULL,
 	cate_number number NOT NULL,
 	PRIMARY KEY (img_num)
@@ -184,8 +184,8 @@ CREATE TABLE image
 CREATE TABLE message
 (
 	message_number number NOT NULL,
-	send_userid varchar2(30) NOT NULL,
-	receive_userid varchar2(30) NOT NULL,
+	send_userid varchar2(200) NOT NULL,
+	receive_userid varchar2(200) NOT NULL,
 	message_content clob NOT NULL,
 	send_Time date NOT NULL,
 	check_Read number,
@@ -196,8 +196,8 @@ CREATE TABLE message
 CREATE TABLE notice_board
 (
 	notice_number number NOT NULL,
-	notice_title varchar2(30) NOT NULL,
-	notice_content varchar2(100),
+	notice_title varchar2(500) NOT NULL,
+	notice_content varchar2(3000),
 	notice_date date,
 	notice_image clob,
 	PRIMARY KEY (notice_number)
@@ -207,12 +207,12 @@ CREATE TABLE notice_board
 CREATE TABLE review
 (
 	review_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	service_number number,
 	star_point number(1),
-	review_content varchar2(100),
-	review_image varchar2(100),
-	review_comment varchar2(100),
+	review_content varchar2(3000),
+	review_image varchar2(3000),
+	review_comment varchar2(3000),
 	cate_number number NOT NULL,
 	review_date date,
 	PRIMARY KEY (review_number)
@@ -223,7 +223,7 @@ CREATE TABLE review_answer
 (
 	review_answer_number number NOT NULL,
 	review_number number NOT NULL,
-	review_content varchar2(100),
+	review_content varchar2(3000),
 	review_answer_Date date,
 	PRIMARY KEY (review_answer_review)
 );
@@ -233,7 +233,7 @@ CREATE TABLE service_answer
 (
 	service_answer_number number NOT NULL,
 	service_question_number number NOT NULL,
-	service_answer_content varchar2(100),
+	service_answer_content varchar2(3000),
 	service_answer_date date,
 	PRIMARY KEY (service_answer_number)
 );
@@ -242,9 +242,9 @@ CREATE TABLE service_answer
 CREATE TABLE service_question
 (
 	service_question_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	service_number number,
-	service_question_content varchar2(50),
+	service_question_content varchar2(3000),
 	service_question_date date,
 	cate_number number NOT NULL,
 	PRIMARY KEY (service_question_number)
@@ -255,9 +255,9 @@ CREATE TABLE tour_info
 (
 	tour_info_number number NOT NULL,
 	service_number number NOT NULL,
-	tour_content varchar2(255),
-	tour_how varchar2(255),
-	tour_rule nvarchar2(255),
+	tour_content varchar2(3000),
+	tour_how varchar2(3000),
+	tour_rule nvarchar2(3000),
 	tour_expire date,
 	PRIMARY KEY (tour_info_number)
 );
@@ -268,7 +268,7 @@ CREATE TABLE tour_option
 	tour_option_number number NOT NULL,
 	service_number number NOT NULL,
 	tour_option_index number NOT NULL,
-	tour_option varchar2(50),
+	tour_option varchar2(3000),
 	tour_price number,
 	tour_amount number,
 	discount number,
@@ -280,24 +280,24 @@ CREATE TABLE tour_service
 (
 	service_number number NOT NULL,
 	cate_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
-	tour_name varchar2(30),
+	user_id varchar2(200) NOT NULL,
+	tour_name varchar2(500),
 	tour_addr clob,
-	tour_type varchar2(100),
+	tour_type varchar2(500),
 	PRIMARY KEY (service_number)
 );
 
 
 CREATE TABLE user_info
 (
-	user_id varchar2(30) NOT NULL,
-	user_pass varchar2(20) NOT NULL,
-	user_name varchar2(20) NOT NULL,
-	user_grade varchar2(20) NOT NULL,
-	user_email varchar2(30),
-	user_addr varchar2(40),
-	user_phone varchar2(255),
-	user_condition varchar2(20),
+	user_id varchar2(200) NOT NULL,
+	user_pass varchar2(3000) NOT NULL,
+	user_name varchar2(200) NOT NULL,
+	user_grade varchar2(100) NOT NULL,
+	user_email varchar2(500),
+	user_addr varchar2(1000),
+	user_phone varchar2(500),
+	user_condition varchar2(200),
 	user_point number,
 	user_enabled char(1) default '1',
 	PRIMARY KEY (user_id)
@@ -310,7 +310,7 @@ CREATE TABLE wishlist
 	wishnum number NOT NULL,
 	cate_number number,
 	service_number number NOT NULL,
-	user_id varchar2(30) NOT NULL,
+	user_id varchar2(200) NOT NULL,
 	PRIMARY KEY (wishnum)
 );
 
@@ -328,9 +328,9 @@ create table visitor_info
 (
     visitor_info_number number not null,
     accom_book_number number not null,
-    visitor_name varchar2(30),
-    visitor_email varchar2(100),
-    visitor_phone varchar2(50),
+    visitor_name varchar2(200),
+    visitor_email varchar2(500),
+    visitor_phone varchar2(100),
     primary key(visitor_info_number)
 );
 
