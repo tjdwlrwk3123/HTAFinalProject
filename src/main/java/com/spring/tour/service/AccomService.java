4ff1a2dc9ee57ad7e2325150404ac325d8c5f5d7
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2021-03-01 21:56:53 +0900

    쓸데없는 쿼리삭제

--- src/main/java/com/spring/tour/service/AccomService.java
+++ src/main/java/com/spring/tour/service/AccomService.java
@@ -70,7 +70,4 @@ public class AccomService {
 	public List<String> accomNameList(String searchValue) {
 		return dao.accomNameList(searchValue);
 	}
-	public HashMap<String, Object> accomMinprice(int service_number){
-		return dao.accomMinprice(service_number);
-	}
 }
